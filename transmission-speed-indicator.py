f83d660548199113dc9d7487644bc88f225a79b8
Cyrille Giquello <cyrille@giquello.fr>
2014-01-05 19:39:55 +0100

    make possible to get network gone and back ;-)

--- transmission-speed-indicator.py
+++ transmission-speed-indicator.py
@@ -22,17 +22,22 @@ from ConfigParser import SafeConfigParser
 
 import logging
 
-PING_FREQUENCY = 60
-
 class EssaiIndicator01:
 
 	PROGRAM_NAME = 'transmission-speed-indicator'
+	CONFIG_FILENAME = 'transmission-speed-indicator.ini'
 	CONFIG_SECTION = 'TRANSMISSION-SPEED-INDICATOR'
+	ICON_NOSERVER = 'transmission-speed-noserver-tray-icon.svg'
+	ICON_HIGH = 'transmission-speed-high-tray-icon.svg'
+	ICON_LOW = 'transmission-speed-low-tray-icon.svg'
 
 	def __init__(self):
 		self.icons_folder = sys.path[0]
+		self.trpc = None
+		self.alt_speed_enabled = False ;
+
 		self.ind = appindicator.Indicator("debian-doc-menu",
-																		 self.icons_folder + "/transmission-speed-tray-icon.svg",
+																		 self.icons_folder + '/' + self.ICON_NOSERVER,
 																		 appindicator.CATEGORY_APPLICATION_STATUS)
 		self.ind.set_status(appindicator.STATUS_ACTIVE)
 		self.menu_setup()
@@ -45,38 +50,79 @@ class EssaiIndicator01:
 		# So I use a variable to avoid an infinite loop ...
 		self.check_running = True
 
-		sess = self.trpc.get_session()
-		#print( "alt speed :", sess.alt_speed_enabled)
+		if self.getTRpc() == None:
+			self.alt_speed_enabled_item.set_inconsistent(True)
+			self.ind.set_icon(self.icons_folder + '/' + self.ICON_NOSERVER )
+			return True
+
+		try :
+			sess = self.trpc.get_session()
+			print( "alt speed :", sess.alt_speed_enabled)
+		except :
+			self.trpc = None
+			return True ;
+
 		if sess.alt_speed_enabled :
-			self.ind.set_icon(self.icons_folder + "/transmission-speed-low-tray-icon.svg")
+			self.ind.set_icon(self.icons_folder + '/' + self.ICON_LOW )
 			if self.alt_speed_enabled_item.get_active() != True :
 				self.alt_speed_enabled_item.set_active(True)
 		else :
-			self.ind.set_icon(self.icons_folder + "/transmission-speed-high-tray-icon.svg")
+			self.ind.set_icon(self.icons_folder + '/' + self.ICON_HIGH )
 			if self.alt_speed_enabled_item.get_active():
 				self.alt_speed_enabled_item.set_active(False)
 
+		self.alt_speed_enabled = sess.alt_speed_enabled
+
 		self.check_running = False
 
-		return sess.alt_speed_enabled
+		return True
 
-	def alt_speed_enabled(self, widget):
+	def menu_alt_speed_enabled(self, widget):
 
-		if self.check_running:
+		if self.check_running or self.trpc == None :
 			return
 
-		if self.check_alt_speed_enabled() :
+		self.check_alt_speed_enabled()
+		if self.alt_speed_enabled :
 			self.trpc.set_session(alt_speed_enabled=False)
 		else :
 			self.trpc.set_session(alt_speed_enabled=True)
 		self.check_alt_speed_enabled()
 
+	def getTRpc(self):
+
+		if self.trpc == None :
+			print( "looking for a server ... "+ self.server)
+			try:
+				self.trpc = transmissionrpc.Client(address=self.server,port=self.port, user=self.user, password=self.password, timeout=self.timeout);
+			except transmissionrpc.error.TransmissionError as e :
+				# do not throw error if server is unreachable
+				# 111 refused connection
+				# 401 basic auth failed
+				if e.original.code != 600 and e.original.code != 101 :
+					#print(e)
+					self.displayErrorAndExit( "Could not access remote transmission server.\nCheck the configuration file '{0}'.\n\n\tError is: {1}\n\nRaw error: {2}".format(self.CONFIG_FILENAME, e.original.message, e.original) )
+
+		return self.trpc
+
+	def displayErrorAndExit(self, error):
+
+		label = gtk.Label( error)
+		label.set_selectable(True)
+		d = gtk.Dialog( self.PROGRAM_NAME, None, gtk.DIALOG_MODAL , (gtk.STOCK_OK, gtk.RESPONSE_ACCEPT) )
+		d.set_border_width(18)
+		d.set_position(gtk.WIN_POS_CENTER)
+		d.vbox.pack_start(label)
+		d.show_all();
+		d.run()
+		sys.exit(0);
+
 	def menu_setup(self):
 		self.menu = gtk.Menu()
 
 		self.alt_speed_enabled_item = gtk.CheckMenuItem("Alt speed")
-		self.alt_speed_enabled_item.connect("activate", self.alt_speed_enabled)
-		#self.alt_speed_enabled_item.connect("toggled", self.alt_speed_enabled)
+		self.alt_speed_enabled_item.connect("activate", self.menu_alt_speed_enabled)
+		#self.alt_speed_enabled_item.connect("toggled", self.menu_alt_speed_enabled)
 		self.alt_speed_enabled_item.show()
 		self.menu.append(self.alt_speed_enabled_item)
 
@@ -88,38 +134,32 @@ class EssaiIndicator01:
 	def quit(self, widget):
 		sys.exit(0)
 
-	def displayErrorAndExit(self, error):
-
-		label = gtk.Label(error)
-		label.show()
-		d = gtk.Dialog( self.PROGRAM_NAME, None, gtk.DIALOG_MODAL , (gtk.STOCK_OK, gtk.RESPONSE_ACCEPT) )
-		d.vbox.pack_start(label)
-		d.run()
-		sys.exit(0);
-
 	def main(self):
 
 		parser = SafeConfigParser()
-		config_filename = self.PROGRAM_NAME+'.ini'
+		
 		try:
-			parser.read(config_filename)
-			server = parser.get(self.CONFIG_SECTION, 'server')
-			port = parser.get(self.CONFIG_SECTION, 'port')
-			user = parser.get(self.CONFIG_SECTION, 'user')
-			password = parser.get(self.CONFIG_SECTION, 'password')
+			parser.read(self.CONFIG_FILENAME)
+			self.server = parser.get(self.CONFIG_SECTION, 'server')
+			self.port = parser.getint(self.CONFIG_SECTION, 'port')
+			self.user = parser.get(self.CONFIG_SECTION, 'user')
+			self.password = parser.get(self.CONFIG_SECTION, 'password')
+			self.timeout = parser.getint(self.CONFIG_SECTION, 'timeout')
+			rpc_interval = parser.getint(self.CONFIG_SECTION, 'rpc_interval')
+			
 		except Exception as e :
-			self.displayErrorAndExit( "Could not read configuration '"+config_filename+"'\nError is: "+e.message)
+			self.displayErrorAndExit( "Could not read configuration '"+self.CONFIG_FILENAME+"'\nError is: "+e.message)
 
-		if parser.has_option(self.CONFIG_SECTION, "trpc_logger_level"):
-			transmissionrpc.utils.add_stdout_logger(parser.get(self.CONFIG_SECTION, 'trpc_logger_level'));
-
-		try:
-			self.trpc = transmissionrpc.Client(address=server,port=port, user=user, password=password);
-		except transmissionrpc.error.TransmissionError as e :
-			self.displayErrorAndExit( 'Could not access remote transmission server.\nError is: '+e.original.message)
+		if parser.has_option(self.CONFIG_SECTION, "rpc_logger_level"):
+			transmissionrpc.utils.add_stdout_logger(parser.get(self.CONFIG_SECTION, 'rpc_logger_level'));
 
 		self.check_alt_speed_enabled()
-		gtk.timeout_add(PING_FREQUENCY * 1000, self.check_alt_speed_enabled)
+
+		# The callback function is called repeatedly until it returns False,
+		# at which point the timeout is automatically destroyed and the function will not be called again
+		# http://www.pygtk.org/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
+		gtk.timeout_add( rpc_interval * 1000, self.check_alt_speed_enabled)
+
 		gtk.main()
 
 if __name__ == "__main__":
